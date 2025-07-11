3db44ec146a8e6bc6520dcaccdef0f72e1e02595
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2025-07-11 18:52:31 +0530

    Create Puzzles.py

---
+++ Puzzles.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+puzzles = list(map(int, input().split()))
+ 
+puzzles.sort()
+min_diff = float('inf')
+ 
+for i in range(m - n + 1):
+    diff = puzzles[i + n - 1] - puzzles[i]
+    min_diff = min(min_diff, diff)
+ 
+print(min_diff)
