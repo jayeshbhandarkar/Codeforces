f02654d42fce9a661cd3b9cd9790667bd5167f13
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2025-07-19 21:16:17 +0530

    Update No_Casino_in_the_Mountains.py

--- No_Casino_in_the_Mountains.py
+++ No_Casino_in_the_Mountains.py
@@ -1,16 +1,29 @@
 t = int(input())
+ 
 for _ in range(t):
     n, k = map(int, input().split())
     a = list(map(int, input().split()))
     
     count = 0
+    zeros = 0
     i = 0
     
-    while i <= n - k:
-        if all(a[i + j] == 0 for j in range(k)):
+    for j in range(k):
+        if a[j] == 0:
+            zeros += 1
+    
+    while i + k <= n:
+        if zeros == k:
             count += 1
-            i += k + 1 
-            
+            i += k + 1
+            if i + k <= n:
+                zeros = sum(1 for x in a[i:i + k] if x == 0)
         else:
+            if i + k < n:
+                if a[i] == 0:
+                    zeros -= 1
+                if a[i + k] == 0:
+                    zeros += 1
             i += 1
+    
     print(count)
